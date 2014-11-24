42c6c919ee44fad104ef9cda82297f450887cb07
bag-man <owg1@aber.ac.uk>
2014-11-24 14:57:46 +0000

    Progress

--- definitions.h
+++ definitions.h
@@ -4,17 +4,19 @@ struct observations_ {
   struct tm time;
   struct observer_ *obersvers;
   struct sighting_ *sightings;
-}
+};
 
 struct observer_ {
   char user_name[4];
   float latitude;
   float longitude;
-}
+};
 
 struct sighting_ {
   struct observer_ observer;
   char mamal;
   float angle;
   float distance;
-}
+};
+
+void read_file(FILE *file, int type);
--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "definitions.h"
 #include "docopt/docopt.c"
-
-struct observations_ observations;
 
 int main(int argc, char *argv[]) {
 
@@ -15,7 +14,33 @@ int main(int argc, char *argv[]) {
   FILE *sightings_file = fopen(args.sightings, "r");
   FILE *observers_file = fopen(args.observers, "r");
 
-
+  read_file(sightings_file, 0);
+  read_file(observers_file, 1);
 
   return 0;
+}
+
+void read_file(FILE *file, int type) {
+  char c = 0;
+  char word[6] = "XXXXXX"; //Is there a less ugly way of doing this?
+  int num_lines = 0;
+
+  while (c != EOF) {
+    c = fscanf(file, "%s", word); 
+    if(c != EOF) {
+      num_lines++;
+      printf("%s\n", word);    
+    }
+  } 
+
+  if(type) {
+    num_lines -= 6;
+    num_lines /= 3;
+  } else {
+    num_lines /= 4;
+  }
+
+  printf("%d", num_lines);
+
+  fclose(file);
 }

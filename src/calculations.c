5677e37fd56379e493528729591bd6676c7a7043
bag-man <owg1@aber.ac.uk>
2014-12-04 16:05:01 +0000

    Adding functionality for part 2

--- src/calculations.c
+++ src/calculations.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#define PROXIMITY 0.02
 #define M_PI 3.14159265358979323846264338327 // Wasn't defined in math.h for some reason
 
 void find_position(Sighting *sighting) {
@@ -11,11 +12,19 @@ void find_duplicates(Sighting *sighting) {
   Sighting *conductor = sighting;
   while(conductor->next != NULL) {
     Sighting *loop_conductor = conductor->next;
+    Sighting *tmp_conductor_list = conductor;
     while(loop_conductor != NULL) {
-      double distance = great_circle(conductor->location, loop_conductor->location);
-      if(distance <= 0.02) {
-	printf("We have a duplicate!\n");
-      } 
+      if(conductor->type == loop_conductor->type) {
+	double distance = great_circle(conductor->location, loop_conductor->location);
+	if(distance <= PROXIMITY) {
+	  printf("We have a duplicate!\n");
+	  // Add duplicate to temp listt
+	  tmp_conductor_list->next = loop_conductor;
+	} 
+      }
+      // Add Average of temp list to sightings list
+      Sighting *average_position;
+      average_position-> // How do I set an average observer!
       loop_conductor = loop_conductor->next;
     }
     loop_conductor = sighting;
